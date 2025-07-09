2d607fd7fe1aa2c066391a1009cede90017d6ef3
Smail Kourta <smail.kourta@canonical.com>
2025-07-09 08:46:57 +0000

    add required provider versions for azure module

--- clouds/azure/versions.tf
+++ clouds/azure/versions.tf
@@ -2,11 +2,28 @@
 # See LICENSE file for licensing details.
 
 terraform {
+  required_version = ">= 1.6"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>4.0"
     }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.7.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~>2.5.3"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~>3.2.4"
+    }
+
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
