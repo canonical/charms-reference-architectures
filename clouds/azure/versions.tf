90913c70bc93f817da5377cf6ec49ec400e9db38
Smail Kourta <smail.kourta@canonical.com>
2025-07-10 10:04:41 +0000

    remove unused provider from versions

--- clouds/azure/versions.tf
+++ clouds/azure/versions.tf
@@ -23,11 +23,6 @@ terraform {
       source  = "hashicorp/null"
       version = "~>3.2.4"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.7.2"
-    }
   }
 
   # set up backend configuration to use Azure Storage Account
