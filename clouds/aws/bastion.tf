5ed007e60ad2c888a3f5b78cc42fa898d188f0f4
reneradoi <rene.radoi@canonical.com>
2025-10-08 21:49:23 +0200

    add setup script

--- clouds/aws/bastion.tf
+++ clouds/aws/bastion.tf
@@ -7,7 +7,6 @@
 
 # security group for bastion access
 resource "aws_security_group" "bastion_sg" {
-  count       = var.PROVISION_BASTION ? 1 : 0
   vpc_id      = aws_vpc.main_vnet.id
 
   ingress {
@@ -38,7 +37,7 @@ resource "aws_security_group" "bastion_to_controller_sg" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.bastion_sg[count.index].id]
+    security_groups = [aws_security_group.bastion_sg.id]
     description     = "SSH access from bastion host"
   }
 
@@ -55,7 +54,6 @@ resource "aws_security_group" "bastion_to_controller_sg" {
 
 
 resource "aws_iam_role" "bastion_role" {
-  count              = var.PROVISION_BASTION ? 1 : 0
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -71,24 +69,21 @@ resource "aws_iam_role" "bastion_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "bastion_role_attachment" {
-  count      = var.PROVISION_BASTION ? 1 : 0
-  role       = aws_iam_role.bastion_role[count.index].name
+  role       = aws_iam_role.bastion_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "bastion_profile" {
-  count   = var.PROVISION_BASTION ? 1 : 0
-  role    = aws_iam_role.bastion_role[count.index].name
+  role    = aws_iam_role.bastion_role.name
 }
 
 resource "aws_instance" "bastion_host" {
-  count                  = var.PROVISION_BASTION ? 1 : 0
   ami                    = "ami-0a116fa7c861dd5f9" #Ubuntu 24.04
   instance_type          = "t2.medium"
   key_name               = var.SSH_KEY
   subnet_id              = aws_subnet.bastion_subnet.id
-  vpc_security_group_ids = [aws_security_group.bastion_sg[count.index].id]
-  iam_instance_profile   = aws_iam_instance_profile.bastion_profile[count.index].name
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.bastion_profile.name
 
   root_block_device {
     volume_type           = "gp2"
@@ -98,4 +93,33 @@ resource "aws_instance" "bastion_host" {
   }
 
   depends_on = [aws_subnet.controller_subnet, aws_iam_instance_profile.bastion_profile]
+}
+
+resource "null_resource" "set_up_bastion_script" {
+  provisioner "file" {
+    content = templatefile("scripts/setup-juju-env.tftpl", {
+      region                 = var.REGION,
+      vpc_id                 = aws_vpc.main_vnet.id,
+      subnet_id              = aws_subnet.controller_subnet.id,
+      access_key             = var.ACCESS_KEY,
+      secret_key             = var.SECRET_KEY,
+    })
+    destination = "setup-juju-env.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash ~/setup-juju-env.sh",
+      "rm ~/setup-juju-env.sh",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.bastion_host.public_ip
+    user        = "ubuntu"
+    private_key = file(var.SSH_KEY)
+  }
+
+  depends_on = [aws_instance.bastion_host]
 }
--- clouds/aws/variables.tf
+++ clouds/aws/variables.tf
@@ -6,20 +6,13 @@ variable "REGION" {
   default = "eu-central-1"
 }
 
-variable "PROVISION_BASTION" {
-  description = "Flag to provision the bastion host"
-  type        = bool
-  default     = true
-}
-
 variable "SOURCE_ADDRESSES" {
   description = "List of IP addresses allowed to SSH into the bastion host"
   type        = list(string)
   default     = null
-  # Needs to be set if PROVISION_BASTION is true
   validation {
-    condition     = var.PROVISION_BASTION == false || (var.PROVISION_BASTION == true && var.SOURCE_ADDRESSES != null)
-    error_message = "SOURCE_ADDRESSES must be set if PROVISION_BASTION is true"
+    condition     = var.SOURCE_ADDRESSES != null
+    error_message = "SOURCE_ADDRESSES must be set"
   }
 }
 
@@ -27,9 +20,28 @@ variable "SSH_KEY" {
   description = "The name of the public key for SSH access to the bastion"
   type        = string
   default     = null
-  # Needs to be set if PROVISION_BASTION is true
   validation {
-    condition     = var.PROVISION_BASTION == false || (var.PROVISION_BASTION == true && var.SSH_KEY != null)
-    error_message = "SSH_KEY must be set if PROVISION_BASTION is true"
+    condition     = var.SSH_KEY != null
+    error_message = "SSH_KEY must be set"
+  }
+}
+
+variable "ACCESS_KEY" {
+  description = "Access key for AWS account"
+  type        = string
+  default     = null
+  validation {
+    condition     = var.ACCESS_KEY != null
+    error_message = "ACCESS_KEY must be set"
+  }
+}
+
+variable "SECRET_KEY" {
+  description = "Secret key for AWS account"
+  type        = string
+  default     = null
+  validation {
+    condition     = var.SECRET_KEY != null
+    error_message = "SECRET_KEY must be set"
   }
 }
