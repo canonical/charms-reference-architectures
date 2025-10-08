1e5206eb6fe4eaa55fe12027189d2b7487c6593c
reneradoi <rene.radoi@canonical.com>
2025-10-08 15:33:56 +0200

    add bastion module

---
+++ clouds/aws/bastion.tf
@@ -0,0 +1,78 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+## ====================================================
+## Provision and set up Bastion Host for juju (Optional)
+## ====================================================
+
+# security group for bastion access
+resource "aws_security_group" "bastion_sg" {
+  vpc_id      = aws_vpc.main_vnet.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.SOURCE_ADDRESSES
+    description = "SSH access from allowed IPs"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  depends_on = [aws_vpc.main_vnet]
+}
+
+resource "aws_iam_role" "bastion_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_role_attachment" {
+  role       = aws_iam_role.bastion_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "bastion_profile" {
+  role = aws_iam_role.bastion_role.name
+}
+
+resource "aws_instance" "bastion_host" {
+  ami                    = "ami-0a116fa7c861dd5f9" #Ubuntu 24.04
+  instance_type          = "t2.medium"
+  key_name               = var.SSH_KEY
+  subnet_id              = aws_subnet.controller_subnet.id
+  vpc_security_group_ids = [aws_security_group.bastion_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.bastion_profile.name
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 8
+    delete_on_termination = true
+    encrypted             = true
+  }
+
+  depends_on = [aws_subnet.controller_subnet, aws_iam_instance_profile.bastion_profile]
+}
+
+resource "aws_eip" "bastion" {
+  domain   = "vpc"
+  instance = aws_instance.bastion_host.id
+
+  depends_on = [aws_instance.bastion_host]
+}
--- clouds/aws/variables.tf
+++ clouds/aws/variables.tf
@@ -5,3 +5,31 @@ variable "REGION" {
   type    = string
   default = "eu-central-1"
 }
+
+variable "PROVISION_BASTION" {
+  description = "Flag to provision the bastion host"
+  type        = bool
+  default     = true
+}
+
+variable "SOURCE_ADDRESSES" {
+  description = "List of IP addresses allowed to SSH into the bastion host"
+  type        = list(string)
+  default     = null
+  # Needs to be set if PROVISION_BASTION is true
+  validation {
+    condition     = var.PROVISION_BASTION == false || (var.PROVISION_BASTION == true && var.SOURCE_ADDRESSES != null)
+    error_message = "SOURCE_ADDRESSES must be set if PROVISION_BASTION is true"
+  }
+}
+
+variable "SSH_KEY" {
+  description = "The name of the public key for SSH access to the bastion"
+  type        = string
+  default     = null
+  # Needs to be set if PROVISION_BASTION is true
+  validation {
+    condition     = var.PROVISION_BASTION == false || (var.PROVISION_BASTION == true && var.SSH_KEY != null)
+    error_message = "SSH_KEY must be set if PROVISION_BASTION is true"
+  }
+}
