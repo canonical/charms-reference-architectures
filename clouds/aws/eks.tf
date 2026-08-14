f356818f2eed49ce20078ff0f670059349c2b3b1
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-08-14 12:26:19 +0200

    patch: add ebs-csi addones

--- clouds/aws/eks.tf
+++ clouds/aws/eks.tf
@@ -125,3 +125,63 @@ resource "aws_eks_node_group" "workers" {
     aws_route_table_association.deployment_clients_private_assoc,
   ]
 }
+
+resource "aws_iam_role" "ebs_csi" {
+  count = var.EKS_CLUSTER_NAME != "" ? 1 : 0
+  name  = "${var.EKS_CLUSTER_NAME}-ebs-csi"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowEksAuthToAssumeRoleForPodIdentity"
+        Effect = "Allow"
+        Principal = {
+          Service = "pods.eks.amazonaws.com"
+        }
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession",
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_AmazonEBSCSIDriverPolicy" {
+  count      = var.EKS_CLUSTER_NAME != "" ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi[count.index].name
+}
+
+resource "aws_eks_addon" "pod_identity_agent" {
+  count        = var.EKS_CLUSTER_NAME != "" ? 1 : 0
+  cluster_name = aws_eks_cluster.eks[count.index].name
+  addon_name   = "eks-pod-identity-agent"
+
+  depends_on = [aws_eks_node_group.workers]
+}
+
+resource "aws_eks_pod_identity_association" "ebs_csi" {
+  count           = var.EKS_CLUSTER_NAME != "" ? 1 : 0
+  cluster_name    = aws_eks_cluster.eks[count.index].name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi[count.index].arn
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.ebs_csi_AmazonEBSCSIDriverPolicy,
+  ]
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  count        = var.EKS_CLUSTER_NAME != "" ? 1 : 0
+  cluster_name = aws_eks_cluster.eks[count.index].name
+  addon_name   = "aws-ebs-csi-driver"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+
+  depends_on = [aws_eks_pod_identity_association.ebs_csi]
+}
