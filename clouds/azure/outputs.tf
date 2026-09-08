ada954e1f042fe235cb8ee530e860a6bce296959
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-09-08 08:46:26 +0200

    patch: (charmed-mongodb) add juju spaces for replica set azure (#31)
    ### Description
    
    This PR adds the setup of 2 subnetworks to the azure cloud deployment.
    This PR adds juju spaces to the charmed-mongodb replica set azure
    solution
    
    We use the [juju
    spaces](https://registry.terraform.io/providers/juju/juju/latest/docs/resources/space)
    and [juju
    subnetwork](https://registry.terraform.io/providers/juju/juju/latest/docs/resources/subnet)
    resources to
    
    - setup 2 spaces: peers and clients 
    - Mongodb is deployed in the peers space
    - Data integrator is deployed in the clients space
    
    ### Juju spaces
     
    <img width="940" height="119" alt="image"
    src="https://github.com/user-attachments/assets/6901c324-2247-4e6e-b8db-18ed0458cae3"
    />
    
    ### Binding addresses
    
    <img width="896" height="563" alt="image"
    src="https://github.com/user-attachments/assets/9333c4f2-17b5-4fcf-9573-bf45d7ae7c86"
    />
    
    ### applications running
    
    <img width="1084" height="925" alt="image"
    src="https://github.com/user-attachments/assets/37da342d-72de-4d44-897c-efa324943756"
    />
    
    ## Note 
    
    I had to use a machine with 4 NICS, otherwise we get this error:
    
    ```
    2        pending           pending        ubuntu@24.04      failed to start machine 2 ({
      "error": {d
        "details": [], 
        "code": "NetworkInterfaceCountExceeded",
        "me...
    3  pending    pending  ubuntu@24.04    failed to start machine 3 ({8
      "error": {
        "details": [],
        "code": "NetworkInterfaceCountExceeded", 
        "me...
    4  pending    pending  ubuntu@24.04    failed to start machine 4 ({
      "error": {
        "details": [],i
        "code": "NetworkInterfaceCountExceeded",-
        "me...
    ```
    
    
    https://learn.microsoft.com/en-us/azure/virtual-machines/sizes/general-purpose/dv3-series?tabs=sizenetwork

--- clouds/azure/outputs.tf
+++ clouds/azure/outputs.tf
@@ -3,11 +3,13 @@
 
 output "infrastructure" {
   value = {
-    resource_group_name     = azurerm_resource_group.main_rg.name
-    vnet_name               = azurerm_virtual_network.main_vnet.name
-    controller_subnet_name  = azurerm_subnet.controller_subnet.name
-    deployments_subnet_name = azurerm_subnet.deployments_subnet.name
-    bastion_public_ip       = var.PROVISION_BASTION ? azurerm_public_ip.bastion_public_ip[0].ip_address : null
+    resource_group_name             = azurerm_resource_group.main_rg.name
+    vnet_name                       = azurerm_virtual_network.main_vnet.name
+    controller_subnet_name          = azurerm_subnet.controller_subnet.name
+    deployments_subnet_name         = azurerm_subnet.deployments_subnet.name
+    deployments_peers_subnet_name   = azurerm_subnet.deployments_peers_subnet.name
+    deployments_clients_subnet_name = azurerm_subnet.deployments_clients_subnet.name
+    bastion_public_ip               = var.PROVISION_BASTION ? azurerm_public_ip.bastion_public_ip[0].ip_address : null
   }
 }
 
